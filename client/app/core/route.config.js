4267b521a125420b42f050df33c4893688331459
Miles Wang <wangxunn@gmail.com>
2015-06-09 10:29:43 -0700

    add example angularjs app structure

--- client/app/core/route.config.js
+++ client/app/core/route.config.js
@@ -1,12 +1,12 @@
 var RouteConfiguration = function ($locationProvider, $stateProvider, $urlRouterProvider) {
-  $locationProvider.html5Mode(true)
+  $locationProvider.html5Mode(true);
   $stateProvider.state("home", {
     url: '/',
     templateUrl: "app/home/home.html",
     controller: "HomeController",
     controllerAs: "vm"
   });
-  $urlRouterProvider.otherwise("/")
+  $urlRouterProvider.otherwise("/");
 };
 
 RouteConfiguration.$inject = [
